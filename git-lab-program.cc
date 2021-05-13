55833670659efe038f8d282290fb3f8117b8c0ae
David Facemyer <df008219@ohio.edu>
2021-05-13 14:23:24 -0400

    My first assigned program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/**
+ * @file: git-lab-program.cc
+ * @author: David Facemyer
+ * @date: May 13, 2021
+ * @brief: My first program example
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///function prototypes
+
+int main(int argc, char const *argv[])  {
+
+   cout << "Hello Git!!" << endl;
+    return 0;
+}/// main
